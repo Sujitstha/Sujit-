7344d28c0574050f1b682c46f761cbc2520dd19d
Sujit Shrestha <sjit.stha@gmail.com>
2021-07-13 10:33:37 +0545

    first commit

---
+++ styling/contacts.js
@@ -0,0 +1,11 @@
+Function lettersOnly(input){
+	var valid = /[]
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<button type="button" onclick="document.getElementById('demo').innerHTML = "Contact">submit!</button>
+
+</body>
+</html>
